1794980d06d090d0158d8f44e3a50dc9a6907927
hi94740 <hi94740@qq.com>
2019-12-03 21:43:26 +0800

    Add Fuzzy Search

---
+++ autocomplete-fixed.js
@@ -0,0 +1,121 @@
+'use strict';
+
+const { Select } = require('enquirer');
+
+const highlight = (input, color) => {
+  let val = input.toLowerCase();
+  return (str, highlightIndices) => {
+    if (highlightIndices) {
+      return highlightIndices.flat().map((h, i) => (i % 2 === 0) ? h : h + 1).map((h, i, a) => {
+        switch (i) {
+          case 0:
+            return [str.slice(0, h), str.slice(h, a[1])];
+          case a.length - 1:
+            return str.slice(h);
+          default:
+            return str.slice(h, a[i + 1]);
+        }
+      }).flat().map((s, i) => (i % 2 === 0) ? s : color(s)).join('');
+    }
+    let s = str.toLowerCase();
+    let i = s.indexOf(val);
+    let colored = color(str.slice(i, i + val.length));
+    return i >= 0 ? str.slice(0, i) + colored + str.slice(i + val.length) : str;
+  };
+};
+
+class AutoComplete extends Select {
+  constructor(options) {
+    super(options);
+    this.cursorShow();
+  }
+
+  moveCursor(n) {
+    this.state.cursor += n;
+  }
+
+  dispatch(ch) {
+    return this.append(ch);
+  }
+
+  space(ch) {
+    return this.options.multiple ? super.space(ch) : this.append(ch);
+  }
+
+  append(ch) {
+    let { cursor, input } = this.state;
+    this.input = input.slice(0, cursor) + ch + input.slice(cursor);
+    this.moveCursor(1);
+    return this.complete();
+  }
+
+  delete() {
+    let { cursor, input } = this.state;
+    if (!input) return this.alert();
+    this.input = input.slice(0, cursor - 1) + input.slice(cursor);
+    this.moveCursor(-1);
+    return this.complete();
+  }
+
+  deleteForward() {
+    let { cursor, input } = this.state;
+    if (input[cursor] === void 0) return this.alert();
+    this.input = `${input}`.slice(0, cursor) + `${input}`.slice(cursor + 1);
+    return this.complete();
+  }
+
+  async complete() {
+    this.completing = true;
+    this.choices = await this.suggest(this.input, this.state._choices.map(ch => { ch.highlightIndices = undefined; return ch; }));
+    this.state.limit = void 0; // allow getter/setter to reset limit
+    this.index = Math.min(Math.max(this.visible.length - 1, 0), this.index);
+    await this.render();
+    this.completing = false;
+  }
+
+  suggest(input = this.input, choices = this.state._choices) {
+    if (typeof this.options.suggest === 'function') {
+      return this.options.suggest.call(this, input, choices);
+    }
+    let str = input.toLowerCase();
+    return choices.filter(ch => ch.message.toLowerCase().includes(str));
+  }
+
+  pointer() {
+    return '';
+  }
+
+  format() {
+    if (!this.focused) return this.input;
+    if (this.options.multiple && this.state.submitted) {
+      return this.selected.map(ch => this.styles.primary(ch.message)).join(', ');
+    }
+    if (this.state.submitted) {
+      let value = this.value = this.input = this.focused.value;
+      return this.styles.primary(value);
+    }
+    return this.input;
+  }
+
+  async render() {
+    if (this.state.status !== 'pending') return super.render();
+    let style = this.options.highlight
+      ? this.options.highlight.bind(this)
+      : this.styles.placeholder;
+
+    let color = highlight(this.input, style);
+    let choices = this.choices;
+    this.choices = choices.map(ch => ({ ...ch, message: color(ch.message, ch.highlightIndices) }));
+    await super.render();
+    this.choices = choices;
+  }
+
+  submit() {
+    if (this.options.multiple) {
+      this.value = this.selected.map(ch => ch.name);
+    }
+    return super.submit();
+  }
+}
+
+module.exports = AutoComplete;
--- screen.js
+++ screen.js
@@ -3,13 +3,20 @@
 const term = require('terminal-kit').terminal
 const shell = require('shelljs')
 const fs = require('fs')
+const AutoComplete = require("./autocomplete-fixed")
 
 const exit = _ => term.processExit()
 
 var ls = shell.exec("screen -ls",{silent:true}).split("\n").map(l => l.trim())
 while(!(ls.pop().split(" ")[1] + "").startsWith("Socket")) {}
 ls.shift()
-ls.unshift("+ Start a new screen...")
+ls = ls.map(l => {
+  return {
+    name:l,
+    message:" > " + l
+  }
+})
+ls.unshift(" + Start a new screen...")
 
 function saveCommand(c) {
   fs.writeFileSync("/tmp/screen-command.sh",c,{mode:0777})
@@ -23,32 +30,63 @@ function err(e) {
 
 
 fs.writeFileSync("/tmp/screen-command.sh","",{mode:0777})
-term.blue("\nThere are ").green((ls.length - 1) + "").blue(ls.length > 2 ? " running screens: ":" running screen: ")
-term.singleColumnMenu(ls,{cancelable:true}).promise.then(result => {
-  if (result.canceled) {
-    saveCommand("")
-  } else {
-    result = result.selectedText
-    if (result == "+ Start a new screen...") {
-      term.blue("\nEnter a name for new screen: ")
-      term.inputField({cancelable:true}).promise.then(result => {
-        if (result == undefined) {
-          saveCommand("")
+term.blue("\nThere are ").green((ls.length - 1) + "").blue(ls.length > 2 ? " running screens: \n":" running screen: \n")
+const seletcted = result => {
+  if (result == " + Start a new screen...") {
+    term.blue("\nEnter a name for new screen: ")
+    term.inputField({cancelable:true}).promise.then(result => {
+      if (result == undefined) {
+        saveCommand("")
+      } else {
+        if (result == "") {
+          term("\n[starting unnamed new screen]\n")
+          saveCommand("screen")
         } else {
-          if (result == "") {
-            term("\n[starting unnamed new screen]\n")
-            saveCommand("screen")
-          } else {
-            term("\n[starting new screen named " + result + "]\n")
-            saveCommand("screen -S " + result)
-          }
+          term("\n[starting new screen named " + result + "]\n")
+          saveCommand("screen -S " + result)
         }
-      }).catch(err)
-    } else {
-      result = result.split("\t")[0]
-      term("\n[attaching to " + result + "]\n")
-      let command = "screen -A -r -x " + result
-      saveCommand(command)
-    }
+      }
+    }).catch(err)
+  } else {
+    result = result.split("\t")[0]
+    term("\n[attaching to " + result + "]\n")
+    let command = "screen -A -r -x " + result
+    saveCommand(command)
   }
-}).catch(err)
+}
+
+
+function loadMenu(message,suggest) {
+  new AutoComplete({
+    name:"menu",
+    message:message,
+    choices:ls,
+    suggest:suggest
+  }).run().then(seletcted).catch(err)
+}
+
+function fuzzySelect() {
+  const Fuse = require("fuse.js")
+  const fuseOptions = {
+    shouldSort: true,
+    includeMatches: true,
+    tokenize: true,
+    threshold: 0.6,
+    location: 0,
+    distance: 100,
+    maxPatternLength: 32,
+    minMatchCharLength: 1,
+    keys: ["message"]
+  }
+  loadMenu("type to filter",(input,list) => {
+    if (input == "") return list
+    let fuse = new Fuse(list, fuseOptions)
+    return fuse.search(input).map(r => {
+      let r1 = r.item
+      r1.highlightIndices = r.matches[0].indices
+      return r1
+    })
+  })
+}
+
+fuzzySelect()
